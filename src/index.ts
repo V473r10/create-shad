a4b479590064701aac8d22569bffd15afa4a590d
V473R10 <punk.road2928@codedicated.com>
2025-09-27 01:31:13 -0300

    Add binary config and make CLI executable

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,40 @@
+#!/usr/bin/env node
+
 import { intro, select, spinner, text } from "@clack/prompts";
 import { createViteProject, sample } from "./frameworks/vite";
 
-intro("Welcome to Create Shad!");
+async function main() {
+  intro("Welcome to Create Shad!");
 
-const framework = await select({
-  message: "Pick a project framework.",
-  options: [
-    { value: "vite", label: "Vite" },
-    { value: "next", label: "Next.js" },
-  ],
-});
+  const framework = await select({
+    message: "Pick a project framework.",
+    options: [
+      { value: "vite", label: "Vite" },
+      { value: "next", label: "Next.js" },
+    ],
+  });
 
-const appName = await text({
-  message: "Enter your app name:",
-  placeholder: "my-app",
-  initialValue: "my-app",
-});
+  const appName = await text({
+    message: "Enter your app name:",
+    placeholder: "my-app",
+    initialValue: "my-app",
+  });
 
-const s = spinner();
+  const s = spinner();
 
-switch (framework) {
-  case "vite":
-    s.start("Creating Vite project...");
-    await createViteProject(appName.toString());
-    s.stop("Vite project created!");
-    s.start("Creating a sample component...");
-    await sample(appName.toString());
-    s.stop("Sample component created!");
-    break;
-  case "next":
-    s.start("Creating Next.js project...");
-    break;
+  switch (framework) {
+    case "vite":
+      s.start("Creating Vite project...");
+      await createViteProject(appName.toString());
+      s.stop("Vite project created!");
+      s.start("Creating a sample component...");
+      await sample(appName.toString());
+      s.stop("Sample component created!");
+      break;
+    case "next":
+      s.start("Creating Next.js project...");
+      break;
+  }
 }
+
+main().catch(console.error);
